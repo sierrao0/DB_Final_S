d37b5df2feda28a04331f47add35ebd5d0f04d89
Sierrao0 <kamisops@gmail.com>
2023-11-14 18:32:04 -0500

    fix

---
+++ sierraBack/src/main/java/com/puj/sierrafinal/SierrafinalConfig.java
@@ -0,0 +1,14 @@
+package com.puj.sierrafinal;
+
+//@Configuration
+public class SierrafinalConfig {
+
+//	@Bean
+//    public Docket api() { 
+//        return new Docket(DocumentationType.SWAGGER_2)  
+//          .select()                                  
+//          .apis(RequestHandlerSelectors.any())              
+//          .paths(PathSelectors.any())                          
+//          .build();                                           
+//    }
+}
